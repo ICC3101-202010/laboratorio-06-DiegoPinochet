878c76cc067099d297b67cb19089dc4b57b9eceb
Diego Pinochet <dppinochet@miuandes.cl>
2020-05-17 10:35:10 -0400

    last commit

--- Lab6_POO_Diego_Pinochet/Division.cs
+++ Lab6_POO_Diego_Pinochet/Division.cs
@@ -9,8 +9,6 @@ namespace Lab6_POO_Diego_Pinochet
     [Serializable]
     public class Division
     {
-        //Mediante una console.readline() ver cual seria la división primaria de la empresa, y entregar el diccionario
-        //correspondiente.
 
         private string name;
         public string Name { get => name; set => name = value; }
